d9f7a5b9b3cdcbccbd47a5f949d3d6762c61b2d4
PatangeDivya <PatangeDivya@users.noreply.github.com>
2024-12-27 12:45:21 +0000

    Fix styling

--- src/FilamentUserCrudPluginServiceProvider.php
+++ src/FilamentUserCrudPluginServiceProvider.php
@@ -10,11 +10,11 @@ use Filament\Support\Facades\FilamentAsset;
 use Filament\Support\Facades\FilamentIcon;
 use Illuminate\Filesystem\Filesystem;
 use Livewire\Features\SupportTesting\Testable;
+use PatangeDivya\FilamentUserCrudPlugin\Commands\FilamentUserCrudPluginCommand;
+use PatangeDivya\FilamentUserCrudPlugin\Testing\TestsFilamentUserCrudPlugin;
 use Spatie\LaravelPackageTools\Commands\InstallCommand;
 use Spatie\LaravelPackageTools\Package;
 use Spatie\LaravelPackageTools\PackageServiceProvider;
-use PatangeDivya\FilamentUserCrudPlugin\Commands\FilamentUserCrudPluginCommand;
-use PatangeDivya\FilamentUserCrudPlugin\Testing\TestsFilamentUserCrudPlugin;
 
 class FilamentUserCrudPluginServiceProvider extends PackageServiceProvider
 {
